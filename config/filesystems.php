cee9ae360b796e0ac5efed2f94f9e2fcdb0e1a31
Valentino Lauciani <valentino.lauciani@ingv.it>
2019-09-24 13:09:30 +0200

    Added 'data' driver

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,11 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
+        
+        'data' => [
+            'driver' => 'local',
+            'root' => storage_path('app')."/".env('DATA_DIR'),
+        ],
 
         'public' => [
             'driver' => 'local',
