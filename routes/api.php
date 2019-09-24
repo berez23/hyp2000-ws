7692ada711cc9b1050a921b99a432547f86edde0
Valentino Lauciani <valentino.lauciani@ingv.it>
2019-09-24 13:10:24 +0200

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function(){
+    Route::post('/hyp2000',         ['uses' => 'App\Api\v1\Controllers\Hyp2000Controller@query', 'middleware' => ['App\Api\Middleware\JsonApiMiddleware']]);
+    Route::get('/hyp2000stations',  ['uses' => 'App\Api\v1\Controllers\Hyp2000StationsController@query', 'middleware' => ['App\Api\Middleware\UniformInputParametersMiddleware']]);
+});
