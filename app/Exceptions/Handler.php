9024656690201173cc4eca63dcdacc7bbdfa41a5
Valentino Lauciani <valentino.lauciani@ingv.it>
2019-09-24 13:08:37 +0200

    Update code to return RFC7807

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,8 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -46,6 +48,71 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        \Log::debug("START - ".__CLASS__.' -> '.__FUNCTION__);
+
+        /* 1/2 - Build array to trigger the Event 'DanteExceptionWasThrownEvent' to send email */
+        $eventArray['url']                  = $request->fullUrl();
+        $eventArray['random_string']        = config('hyp2000-ws.random_string');
+        $eventArray['log_file']             = config('hyp2000-ws.log_file');
+        
+        /* 1/2 - Build RCF7807 Problem Details for HTTP APIs: https://tools.ietf.org/html/rfc7807 */
+        $rcf7807Output['type']              = 'unknown';
+        $rcf7807Output['title']             = 'unknown';
+        $rcf7807Output['status']            = 500;
+        $rcf7807Output['detail']            = 'unknown';
+        $rcf7807Output['instance']          = $request->fullUrl();
+        $rcf7807Output['version']           = config('hyp2000-ws.version');
+        $rcf7807Output['request_submitted'] = date("Y-m-d\TH:m:s T");
+        
+        /* Set header to get JSON render exception */
+        $request->headers->set('Accept', 'application/json');
+
+        /* Get default render */
+        $defaultRender = parent::render($request, $exception);
+
+        /* Get status, status code and status message */
+        $status                             = (parent::isHttpException($exception) ? $exception->getStatusCode() : ($defaultRender->getStatusCode() ? $defaultRender->getStatusCode() : 500));
+        $statusMessage                      = Response::$statusTexts[$status] ? Response::$statusTexts[$status] : '--';
+        $message                            = $defaultRender->getData()->message;        
+
+        /* 2/2 - Build RCF7807 Problem Details for HTTP APIs: https://tools.ietf.org/html/rfc7807 */
+        $rcf7807Output['type']              = config('hyp2000-ws.rfc7231')[$status] ?? 'about:blank';
+        $rcf7807Output['title']             = $statusMessage;
+        $rcf7807Output['status']            = $status;
+        $rcf7807Output['detail']            = $message ? $message : $statusMessage;
+        
+        /* 2/2 - Build array to trigger the Event 'DanteExceptionWasThrownEvent' to send email */
+        $eventArray['message']              = $exception->getMessage() ? $exception->getMessage() : '--';
+        $eventArray['status']               = $status;
+        $eventArray['statusMessage']        = $statusMessage;
+        $eventArray['message']              .= ' - '.$exception->getFile().':'.$exception->getLine();
+
+        /* Set header to 'application/problem+json' */
+        $defaultRender->header('Content-type','application/problem+json');
+
+        /* Set errors array */ 
+        if (isset($defaultRender->getData()->errors)) {
+            $rcf7807Output['errors']     = (array)$defaultRender->getData()->errors;
+        }
+
+        /* Add debug */
+        if ( config('app.debug') ) {
+            $rcf7807Output['debug']     = (array)$defaultRender->getData();
+        }
+
+        /* Set output with new fields */
+        $defaultRender->setData($rcf7807Output);
+
+        /* set output */
+        $prepareOutput = $defaultRender;
+
+        /* print into log */
+        \Log::debug(" exception:", $rcf7807Output);
+
+        /* Trigger the event */
+        //Event::fire(new DanteExceptionWasThrownEvent($eventArray));
+
+        \Log::debug("END - ".__CLASS__.' -> '.__FUNCTION__);
+        return $prepareOutput;
     }
 }
