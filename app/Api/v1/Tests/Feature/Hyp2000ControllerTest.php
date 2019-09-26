9dd309ce392e6b48dfd3928bf47964b313e27aa9
Valentino Lauciani <valentino.lauciani@ingv.it>
2019-09-26 16:25:28 +0200

    Update code removing old dependencies

--- app/Api/v1/Tests/Feature/Hyp2000ControllerTest.php
+++ app/Api/v1/Tests/Feature/Hyp2000ControllerTest.php
@@ -6,15 +6,17 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Api\Traits\UtilsTrait;
 use App\Api\v1\Tests\Feature\Hyp2000Input;
 use App\Api\v1\Tests\Feature\Hyp2000OutputJson;
 
 class Hyp2000ControllerTest extends TestCase
 {
+    use UtilsTrait;
+    
     protected $uri                          = '/api/v1/hyp2000';
     protected $hyp2000Input;
     protected $hyp2000OutputJsonExpected;
-    
     
     public function setUp(): void {
         parent::setUp();
@@ -38,8 +40,7 @@ class Hyp2000ControllerTest extends TestCase
         $this->assertContains($response->status(), [200], $response->content());
         
         // Init class to convert in structure
-        $IngvControllerTest_class = new IngvControllerTest();              
-        $hyp2000OutputJsonExpected__json__structure = $IngvControllerTest_class->getArrayStructure(json_decode($response->content(), true));        
+        $hyp2000OutputJsonExpected__json__structure = $this->getArrayStructure(json_decode($response->content(), true));        
         
         /* Check structure */
         $response->assertJsonStructure($hyp2000OutputJsonExpected__json__structure);
